3e64b91e5a6d90454f6cb9cf7cbe3ef7aafc0e17
stylelinz <cwchuang1997@gmail.com>
2021-03-27 21:08:46 +0800

    add: card symbols, render(View) funtions +getCardElement, transformNumber #20210327001

--- main.js
+++ main.js
@@ -1,12 +1,41 @@
+const Symbols = [
+  'https://image.flaticon.com/icons/svg/105/105223.svg', // spade
+  'https://image.flaticon.com/icons/svg/105/105220.svg', // heart
+  'https://image.flaticon.com/icons/svg/105/105212.svg', // diamonds
+  'https://image.flaticon.com/icons/svg/105/105219.svg'  // club
+]
+
 const view = {
-  displayCards() {
-    document.querySelector('#cards').innerHTML = `
+  getCardElement(index) {
+    const number = this.transformNumber((index % 13) + 1)
+    const cardSymbol = Symbols[Math.floor(index / 13)]
+    return `
     <div class="card">
-      <p>3</p>
-      <img src="https://image.flaticon.com/icons/svg/105/105219.svg" alt="spare">
-      <p>3</p>
+      <p>${number}</p>
+      <img src="${cardSymbol}" />
+      <p>${number}</p>
     </div>
     `
+  },
+
+  transformNumber(number) {
+    switch (number) {
+    case 1:
+      return 'A'
+    case 11:
+      return 'J'
+    case 12:
+      return 'Q'
+    case 13:
+      return 'K'
+    default:
+      return number
+    }
+  },
+
+  displayCards() {
+    const rootElement = document.querySelector('#cards')
+    rootElement.innerHTML = this.getCardElement(10)
   }
 }
 view.displayCards()
